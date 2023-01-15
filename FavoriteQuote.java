0436b5f74583753380246a75bc5c09e933db07d6
heybeals <121995767+heybeals@users.noreply.github.com>
2023-01-15 13:49:43 -0800

    Add files via upload

---
+++ FavoriteQuote.java
@@ -0,0 +1,9 @@
+/* 
+Favorite Quote 
+*/
+class FavoriteQuote {
+        public static void main(String[] args) {
+        String output= "Life is ten percent what happens to you and ninety percent how you respond to it.";
+        System.out.println(output);
+    }
+}
